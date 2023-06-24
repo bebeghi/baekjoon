e94344d67e34d8a844428d5d652e81eb500363cf
bebeghi <129737525+bebeghi@users.noreply.github.com>
2023-06-24 10:26:22 +0900

    Create 백준9498.c
    시험 성적
    
    시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.

---
+++ 백준9498.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+    int Score;
+    scanf("%d", &Score);
+    
+    if (Score >= 90 && Score <= 100)
+    {
+        printf("A");
+    }
+    else if (Score >= 80 && Score <= 89){
+        printf("B");
+    }
+    else if (Score >= 70 && Score <= 79){
+        printf("C");
+    }
+    else if (Score >= 60 && Score <= 69){
+        printf("D");
+    }
+    else {
+        printf("F");
+    }
+}
